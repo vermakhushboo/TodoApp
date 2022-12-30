fb22088cb67ba286f4b35aee15db90d4579be295
Khushboo Verma <khushbooverma@Khushboos-MacBook-Pro.local>
2022-12-30 21:12:53 +0530

    Add view engine config & post request

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const app = express();
 
 app.use("/static", express.static("public"));
 
+//extract data from form and add to res body
+app.use(express.urlencoded({extended: true}));
+
+//View Engine Configuration
 app.set("view engine", "ejs");
 
+//GET Method
 app.get('/', (req, res) => {
     res.render("todo.ejs");
+})
+
+//POST Method
+app.post('/', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(3000, () => console.log("Server up and running!"));

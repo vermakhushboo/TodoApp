c88bb7f27088fe9d4867e1f3ff6f7ddbb7bb9100
Khushboo Verma <khushbooverma@Khushboos-MacBook-Pro.local>
2022-12-30 22:12:37 +0530

    Connect to database

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+//models
+const TodoTask = require("./models/TodoTask");
+
+dotenv.config();
 
 app.use("/static", express.static("public"));
 
 //extract data from form and add to res body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
+//connection to db
+mongoose.connect(process.env.DB_CONNECT, () => {
+  console.log("Connected to db!");
+  app.listen(3000, () => console.log("Server Up and running"));
+});
 
 //View Engine Configuration
 app.set("view engine", "ejs");
 
 //GET Method
-app.get('/', (req, res) => {
-    res.render("todo.ejs");
-})
+app.get("/", (req, res) => {
+  res.render("todo.ejs");
+});
 
 //POST Method
-app.post('/', (req, res) => {
-    console.log(req.body);
-})
-
-app.listen(3000, () => console.log("Server up and running!"));
+app.post("/", async (req, res) => {
+  const todoTask = new TodoTask({
+    content: req.body.content,
+  });
+  try {
+    await todoTask.save();
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
